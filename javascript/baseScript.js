285d0bd93cff9f98cb4d234158338bb62c4dbf9f
Goutai Phan <sb.phanquocthai@gmail.com>
2022-02-06 20:54:54 +0700

    commit

---
+++ javascript/baseScript.js
@@ -0,0 +1,33 @@
+//document.write(String(window.innerWidth));
+let serviceArea = document.getElementById('serviceArea');
+let serviceData = getFileData('document/PhauThuatThamMyVI.txt');
+let serviceParent = getFileArray(serviceData, 0);
+let serviceChild = getFileArray(serviceData, 1);
+let serviceDataEN = getFileData('document/PhauThuatThamMyEN.txt');
+let serviceChildEN = getFileArray(serviceDataEN, 1);
+
+for (let i = 0; i < serviceParent.length; i++) {
+    let serviceHead = document.createElement('img');
+    serviceArea.appendChild(serviceHead);
+    serviceHead.id = 'serviceHead';
+    serviceHead.src = `asset/Service_${deAccent(serviceParent[i])}.png`;
+
+    let serviceBody = document.createElement('div');
+    serviceBody.id = 'serviceBody';
+    serviceArea.appendChild(serviceBody);
+
+    for (let j = 0; j < serviceChild[i].length; j++) {
+        let service = document.createElement('div');
+        let serviceVI = document.createElement('p');
+        let serviceEN = document.createElement('p');
+        serviceBody.appendChild(service);
+        service.appendChild(serviceVI);
+        service.appendChild(serviceEN);
+
+        service.id = 'service';
+        serviceVI.id = 'serviceVI';
+        serviceEN.id = 'serviceEN';
+        serviceVI.innerHTML = serviceChild[i][j];
+        serviceEN.innerHTML = serviceChildEN[i][j];
+    }
+}
---
+++ javascript/functionScript.js
@@ -0,0 +1,38 @@
+function getFileArray(fileData, fileType) {
+    let fileTitle = fileData.split('|')[0];
+    let fileContent = fileData.split('|')[1];
+
+    let fileParent = fileTitle.replaceAll('<br>', ' ')
+        .split('\n').filter(item => item);
+
+    let fileChild = [];
+    for (let i = 0; i < fileParent.length; i++) {
+        let startIndex = fileContent.indexOf(fileParent[i]) + fileParent[i].length;
+        let endIndex = i === fileParent.length - 1
+            ? fileContent.length
+            : fileContent.indexOf(fileParent[i + 1]);
+        let array = fileContent.substring(startIndex, endIndex)
+            .split('\n').filter(item => item);
+        fileChild.push(array);
+    }
+
+    return fileType === 0
+        ? fileParent
+        : fileChild;
+}
+
+function deAccent(string) {
+    let normalization = string.normalize('NFD').replace(/[\u0300-\u036f]/g, "");
+    return normalization.replace(/ \D/g, function (match) {
+        return match.toUpperCase().replaceAll(' ', '');
+    });
+}
+
+function getFileData(url) {
+    let request = new XMLHttpRequest();
+    request.open('get', url, false);
+    request.send(null);
+    return request.responseText
+        .replaceAll('\t', '')
+        .replaceAll('\r\n', '\n');
+}
